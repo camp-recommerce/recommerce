32c493c4969229a422ff8e636f97c404b5be92f4
ljwc6020 <156625913+ljwc6020@users.noreply.github.com>
2024-04-24 12:19:29 +0900

    알림 수정 완료
    css 알림 읽음처리, 알림을 다 읽고도 알림을 보낸 유저와 채팅 모달 열 수 있도록 수정

--- recommerceFront/src/components/modal/AlarmModal.js
+++ recommerceFront/src/components/modal/AlarmModal.js
@@ -8,7 +8,7 @@ import { readAlarms } from "../../api/chatAlarmApi";
 const AlarmModal = ({ closeModal, email }) => {
   const { openChatModal, isChatModalOpen, socket } = useCustomChatModal(); // closeChatModal 함수 불러오기
   const { loginState } = useCustomLogin();
-  const { alarmList: originalAlarmList } = useCustomChatAlarm(); // 기존의 alarmList
+  const { alarmList: originalAlarmList, refreshAlarm } = useCustomChatAlarm(); // 기존의 alarmList
 
   const [groupedAlarms, setGroupedAlarms] = useState({}); // 그룹화된 알람
   const [newAlarmList, setNewAlarmList] = useState(originalAlarmList);
@@ -20,21 +20,22 @@ const AlarmModal = ({ closeModal, email }) => {
   };
 
   const handleReadAlarms = async (senderEmail) => {
-    // roomId에 해당하는 알림들을 가져옵니다.
     const alarmsInRoom = groupedAlarms[senderEmail];
-    console.log(groupedAlarms);
-
-    // 만약 roomId에 해당하는 알림이 없다면 처리 중단
     if (!alarmsInRoom) {
-      console.log("No alarms found for the provided roomId.");
+      console.log("No alarms found for the provided senderEmail.");
       return;
     }
+
     // 해당 roomId에 해당하는 알림들의 id 배열을 추출합니다.
     const alarmIds = alarmsInRoom.map((alarm) => alarm.id);
 
     // 추출된 알람들의 id 배열을 전달하여 알람 상태를 업데이트합니다.
-    await readAlarms(alarmIds);
+    readAlarms(alarmIds);
+    refreshAlarm();
     console.log("Alarms marked as read.");
+
+    // 채팅 모달 열기
+    openChatModal(alarmsInRoom[0].roomId);
   };
 
   useEffect(() => {
@@ -63,7 +64,9 @@ const AlarmModal = ({ closeModal, email }) => {
             className="text-blue-500 hover:underline"
             onClick={() => {
               const roomId = groupedAlarms[senderEmail][0].roomId;
+              console.log(roomId);
               openChatModal(roomId);
+              console.log(isChatModalOpen);
               handleReadAlarms(senderEmail);
             }}
           >
@@ -71,24 +74,24 @@ const AlarmModal = ({ closeModal, email }) => {
           </button>
         </h3>
         <div>
+          {isChatModalOpen && (
+            <Chat
+              room={groupedAlarms[senderEmail][0].roomId} // 해당 발신자의 roomId 사용
+              username={loginState.email} // 발신자의 이메일 전달
+              socket={socket}
+              closeModal={() => {
+                closeModal();
+              }}
+            />
+          )}
+        </div>
+        <div>
           {groupedAlarms[senderEmail]
             .filter((alarm) => !alarm.readCheck) // 읽지 않은 알람만 필터링
             .map((alarm, subIndex) => (
               <div key={subIndex} className="border-b border-gray-200 py-2">
                 <p>시간: {alarm.createdAt}</p>
                 <p>메시지: {alarm.message}</p>
-                <div>
-                  {isChatModalOpen && (
-                    <Chat
-                      room={alarm.roomId} // 해당 발신자의 roomId 사용
-                      username={loginState.email} // 발신자의 이메일 전달
-                      socket={socket}
-                      closeModal={() => {
-                        closeModal();
-                      }}
-                    />
-                  )}
-                </div>
               </div>
             ))}
         </div>
