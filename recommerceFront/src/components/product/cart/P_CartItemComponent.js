daab37baf3bc651e943f49bb92c6358902ba2c4d
ljwc6020 <156625913+ljwc6020@users.noreply.github.com>
2024-04-29 13:47:19 +0900

    Update P_CartItemComponent.js
    장바구니 css 수정

--- recommerceFront/src/components/product/cart/P_CartItemComponent.js
+++ recommerceFront/src/components/product/cart/P_CartItemComponent.js
@@ -13,15 +13,13 @@ const P_CartItemComponent = ({
   email,
   soldOut,
   userEmail,
-
 }) => {
   const host = API_SERVER_HOST;
 
-  const {moveCartToProductRead} =
-    useCustomMovePage();
+  const { moveCartToProductRead } = useCustomMovePage();
 
   const handleDelete = () => {
-    changeCart({ email, wino, pno, qty: 0});
+    changeCart({ email, wino, pno, qty: 0 });
   };
 
   return (
@@ -29,7 +27,7 @@ const P_CartItemComponent = ({
       <div
         key={pno}
         className="shopList_wrap"
-        style={{ width: 400, height: 320, marginLeft: 20, marginTop: 10 }} // 검은색 1px 짜리 테두리
+        style={{ width: 350, height: 320, marginLeft: 20, marginTop: 10 }} // 검은색 1px 짜리 테두리
         onMouseEnter={(e) => {
           e.currentTarget.querySelector(".shopList_box").style.transform =
             "scale(1.05)";
@@ -49,30 +47,25 @@ const P_CartItemComponent = ({
             <img
               alt={pname}
               src={`${host}/product/view/s_${imageFile}`}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain"
               onClick={() => moveCartToProductRead(pno)}
-              style={{ width: 350, height: 200 }}
+              style={{ width: 350, height: 250 }} // Adjusted style
             />
           </div>
           <div className="shopList_pname text-sm mb-1 flex justify-center">
-          상품명: {pname} ({soldOut ? '판매완료' : '판매중'})
+            상품명: {pname} ({soldOut ? "판매완료" : "판매중"})
           </div>
           <div className="shopList_pname text-sm mb-1 flex justify-center">
-          판매자: {userEmail}
+            판매자: {userEmail}
           </div>
           <div className="shopList_pname text-sm mb-1 flex justify-center">
-          가격: {formatNumber(price)}원
+            가격: {formatNumber(price)}원
           </div>
-            
-            <div className="shopList_end text-sm">
-              
 
-            </div>
-          </div>
+          <div className="shopList_end text-sm"></div>
         </div>
-
+      </div>
     </>
   );
-  
 };
 export default P_CartItemComponent;
